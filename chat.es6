79f715d05cf4597229c173f41dfe5b23af5e3c23
Vihan Bhargava <blue1086@gmail.com>
2016-02-03 22:43:20 -0800

    Final changed rate limiting

--- chat.es6
+++ chat.es6
@@ -10,6 +10,7 @@ class Chatbot {
     this.Options = { Startup: Data(Startup, this), UID };
     this.onmessage = onmessage || () => void 0;
 
+    this.Queue = [];
     // Private
     let Called = Symbol("Called");
 
@@ -23,7 +24,8 @@ class Chatbot {
       [...document.getElementsByClassName("message")].filter((message, index, transcript) => {
         if (this[Called].has(message)) return false;
         else this[Called].add(message);
-        return ( message.parentElement.parentElement.className.match(/user-(\d+)/) || [0, UID] )[1] != UID;
+        return ( message.parentElement.parentElement.className.match(/user-(\d+)/) || [0, UID] )[1] != UID &&
+          index > transcript.map(t => t.textContent.trim()).lastIndexOf(this.Options.Startup)
       }).forEach(message => this.onmessage.call({
         "Text": message.textContent.trim().replace(/([a-z]\.)+(?:com|org|net|xyz)/g, "http://$&"),
         "HTML": message.innerHTML,
@@ -31,13 +33,15 @@ class Chatbot {
 
         "User": message.parentElement.parentElement.getElementsByClassName("username")[0].textContent.trim(),
 
-        "Speak": this.Speak,
-        "Reply": this.Reply(message)
-      }, this));
+        "Speak": Text => this.Queue.push(Text),
+        "Reply": Text => this.Queue.push(`:${message.id.split("-")[1]} ${Text}`)
+      }));
     }, 2100);
+
+    setInterval(() => {
+      if (this.Queue[0]) Talk(this.Queue.shift());
+    }, 1000);
   }
-  Speak(text) { Talk(text) }
-  Reply(message) { return Text => Talk(`:${message.id.split("-")[1]} ${Text}`) }
 }
 
 /*
@@ -48,23 +52,22 @@ let Admins = new Set(["Doᴡɴɢᴏᴀᴛ", "Chatgoat"]);
 let Commands = {
   "help": args => Object.keys(Commands).join(", "),
   "learn": (args, c) => ( Commands[ args[0] ] = () => Data(args.slice(1).join(" "), c), `Learned how to ${args[0]}!`),
-  
+
   // Random functions
   "golf": args => {
     let len = Math.floor(Math.random()*args.join(" ").length*.5)+1;
     return Array(len).fill().map(() => String.fromCharCode(Math.floor(Math.random()*1200+255))).join("") + " is only " + 
       (Math.floor(Math.random() * (len - 1)) + 1) + " bytes in the " +
       Array(Math.floor(Math.random() * 2) + 2).fill().map(() => String.fromCharCode(Math.floor(Math.random() * 25) + 65)).join("") + "-" +
-      Math.floor(Math.random()*Math.pow(10, Math.floor(Math.random() * 4) + 2)).join("") +
+      Math.floor(Math.random()*Math.pow(10, Math.floor(Math.random() * 4) + 2)) +
       " encoding"
   }
 };
 
-let Chatgoat = new Chatbot("Chatgoat", { UID: 180858, Startup: "Hello! My name is $Name!" }, function(self) {
+let Chatgoat = new Chatbot("Chatgoat", { UID: 180858, Startup: "Hello! My name is $Name!" }, function() {
   if (this.Text[0] === "/") { // Command
     let Command = this.Text.split(" ")[0].slice(1); // Command
     this.Speak(
-      ( this.Text.split(" ").reverse()[1] === "to" ? `@${this.Text.split(" ").reverse()[0]} ` : "" ) +
       Commands[Command](this.Text.split(" ").slice(1), this)
     );
   } else {
--- chat.js
+++ chat.js
@@ -4,8 +4,6 @@
 // Helper Functions
 "use strict";
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -19,75 +17,69 @@ var Data = function Data(text, instance) {
   });
 };
 
-var Chatbot = (function () {
-  function Chatbot(Name, _x, onmessage) {
-    if (Name === undefined) Name = "a Chatbot";
+var Chatbot = function Chatbot(Name, _x, onmessage) {
+  if (Name === undefined) Name = "a Chatbot";
 
-    var _this = this;
+  var _this = this;
 
-    var _ref = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+  var _ref = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
 
-    var _ref$Startup = _ref.Startup;
-    var Startup = _ref$Startup === undefined ? "Hi my name is $Name!" : _ref$Startup;
-    var _ref$UID = _ref.UID;
-    var UID = _ref$UID === undefined ? 0 : _ref$UID;
+  var _ref$Startup = _ref.Startup;
+  var Startup = _ref$Startup === undefined ? "Hi my name is $Name!" : _ref$Startup;
+  var _ref$UID = _ref.UID;
+  var UID = _ref$UID === undefined ? 0 : _ref$UID;
 
-    _classCallCheck(this, Chatbot);
+  _classCallCheck(this, Chatbot);
 
-    this.Name = Name;
-    this.Options = { Startup: Data(Startup, this), UID: UID };
-    this.onmessage = onmessage || function () {
-      return void 0;
-    };
+  this.Name = Name;
+  this.Options = { Startup: Data(Startup, this), UID: UID };
+  this.onmessage = onmessage || function () {
+    return void 0;
+  };
 
-    // Private
-    var Called = Symbol("Called");
+  this.Queue = [];
+  // Private
+  var Called = Symbol("Called");
 
-    this[Called] = new Set();
+  this[Called] = new Set();
 
-    // Startup
-    Talk(this.Options.Startup);
+  // Startup
+  Talk(this.Options.Startup);
 
-    // Read
-    setInterval(function () {
-      [].concat(_toConsumableArray(document.getElementsByClassName("message"))).filter(function (message, index, transcript) {
-        if (_this[Called].has(message)) return false;else _this[Called].add(message);
-        return (message.parentElement.parentElement.className.match(/user-(\d+)/) || [0, UID])[1] != UID;
-      }).forEach(function (message) {
-        return _this.onmessage.call({
-          "Text": message.textContent.trim().replace(/([a-z]\.)+(?:com|org|net|xyz)/g, "http://$&"),
-          "HTML": message.innerHTML,
-          "Raw": message,
+  // Read
+  setInterval(function () {
+    [].concat(_toConsumableArray(document.getElementsByClassName("message"))).filter(function (message, index, transcript) {
+      if (_this[Called].has(message)) return false;else _this[Called].add(message);
+      return (message.parentElement.parentElement.className.match(/user-(\d+)/) || [0, UID])[1] != UID && index > transcript.map(function (t) {
+        return t.textContent.trim();
+      }).lastIndexOf(_this.Options.Startup);
+    }).forEach(function (message) {
+      return _this.onmessage.call({
+        "Text": message.textContent.trim().replace(/([a-z]\.)+(?:com|org|net|xyz)/g, "http://$&"),
+        "HTML": message.innerHTML,
+        "Raw": message,
 
-          "User": message.parentElement.parentElement.getElementsByClassName("username")[0].textContent.trim(),
+        "User": message.parentElement.parentElement.getElementsByClassName("username")[0].textContent.trim(),
 
-          "Speak": _this.Speak,
-          "Reply": _this.Reply(message)
-        }, _this);
+        "Speak": function Speak(Text) {
+          return _this.Queue.push(Text);
+        },
+        "Reply": function Reply(Text) {
+          return _this.Queue.push(":" + message.id.split("-")[1] + " " + Text);
+        }
       });
-    }, 2100);
-  }
+    });
+  }, 2100);
 
-  /*
-   * CHATGOAT v2.0
-  **/
+  setInterval(function () {
+    if (_this.Queue[0]) Talk(_this.Queue.shift());
+  }, 1000);
+}
 
-  _createClass(Chatbot, [{
-    key: "Speak",
-    value: function Speak(text) {
-      Talk(text);
-    }
-  }, {
-    key: "Reply",
-    value: function Reply(message) {
-      return function (Text) {
-        return Talk(":" + message.id.split("-")[1] + " " + Text);
-      };
-    }
-  }]);
-
-  return Chatbot;
-})();
+/*
+ * CHATGOAT v2.0
+**/
+;
 
 var Admins = new Set(["Doᴡɴɢᴏᴀᴛ", "Chatgoat"]);
 
@@ -112,11 +104,11 @@ var Commands = {
   }
 };
 
-var Chatgoat = new Chatbot("Chatgoat", { UID: 180858, Startup: "Hello! My name is $Name!" }, function (self) {
+var Chatgoat = new Chatbot("Chatgoat", { UID: 180858, Startup: "Hello! My name is $Name!" }, function () {
   if (this.Text[0] === "/") {
     // Command
     var Command = this.Text.split(" ")[0].slice(1); // Command
-    this.Speak((this.Text.split(" ").reverse()[1] === "to" ? "@" + this.Text.split(" ").reverse()[0] + " " : "") + Commands[Command](this.Text.split(" ").slice(1), this));
+    this.Speak(Commands[Command](this.Text.split(" ").slice(1), this));
   } else {
     if (/(Hello|Hi|Hey)(?=[^A-Za-z])/i.test(this.Text)) {
       this.Reply("Hello " + this.User + "!");
